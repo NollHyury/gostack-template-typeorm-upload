9b64d71f44d09ea6496b0487c763c9307f53bd87
Mr. Noll <nollhyury@gmail.com>
2020-04-29 21:43:58 -0300

    new features

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,54 @@
-// import AppError from '../errors/AppError';
-
+import { getRepository, getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface TransactionDTO {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  categoryString: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    type,
+    value,
+    categoryString,
+  }: TransactionDTO): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const balance = await transactionRepository.getBalance();
+    if (type === 'income' || type === 'outcome') {
+      if (type === 'outcome' && balance.total < value) {
+        throw new AppError(
+          'value of outcome is most expensive than your income value!',
+          400,
+        );
+      }
+      const categoryRepository = getRepository(Category);
+
+      const category = await categoryRepository.findOne({
+        where: { title: categoryString },
+      });
+      const transaction = transactionRepository.create({
+        title,
+        value,
+        type,
+        category,
+      });
+      if (!category) {
+        const newCategory = categoryRepository.create({
+          title: categoryString,
+        });
+        await categoryRepository.save(newCategory);
+        transaction.category = newCategory;
+      }
+      await transactionRepository.save(transaction);
+      return transaction;
+    }
+    throw new AppError('Type of transaction is not valid', 400);
   }
 }
 
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,23 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import Transaction from '../models/Transaction';
+
+interface TransactionDeleteDTO {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: TransactionDeleteDTO): Promise<void> {
+    const transactionRepository = getRepository(Transaction);
+    const transaction = await transactionRepository.findOne({
+      where: { id },
+    });
+
+    if (!transaction) {
+      throw new AppError('Transaction not found!', 404);
+    }
+
+    await transactionRepository.remove(transaction);
   }
 }
 
